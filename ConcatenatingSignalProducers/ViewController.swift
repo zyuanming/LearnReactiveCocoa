afa2269ce18797105c7575a5d04a8ee4815e9b48
Yuanming Zhang <zhangyuanming@baidu.com>
2017-07-18 15:57:08 +0800

    add SignalProducer combine latest test

--- ConcatenatingSignalProducers/ViewController.swift
+++ ConcatenatingSignalProducers/ViewController.swift
@@ -45,55 +45,62 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        // Test strategy concate
+//        let test = TestConcate()
+//        test.testPropertyToSignalProducer()
+//
+//        var a: TestSignalDeinit?
+//        a = TestSignalDeinit()
+//        a = nil
+//
+        TestSignalAndObserver()
+
+        TestCombineLatest().test()
+    }
+
+
+
+}
+
+
+class TestDownload {
+    func test() {
         let client = APIClient()
-        
+
         let users = [
             User(name: "User 1", avatarUrl: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1794894692,1423685501&fm=116&gp=0.jpg"),
             User(name: "User 2", avatarUrl: "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=17572568,2472534097&fm=116&gp=0.jpg"),
             User(name: "User 3", avatarUrl: "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1303680113,133301350&fm=116&gp=0.jpg"),
             User(name: "User 4", avatarUrl: "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1425317030,4113620941&fm=116&gp=0.jpg"),
             User(name: "User 5", avatarUrl: "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1237679276,3516486176&fm=116&gp=0.jpg"),
-        ]
-        
+            ]
+
         let getAvatars = users.map { client.getAvatar(url: $0.avatarUrl) }
-        
+
         // Flatten strategy: concate, latest, merge
-//        SignalProducer
-//            .flatten(.concat, producers: getAvatars)
-//            .on(starting: {
-//                print("starting")
-//            }, started: {
-//                print("started")
-//            }, event: { event in
-//                print("event is \(event)")
-//            }, value: { image in
-//                print("value is \(image)")
-//            }, completed: {
-//                print("completed")
-//            },  disposed: {
-//                print("disposed")
-//            })
-//            .startWithResult { result in
-//                switch result {
-//                case let .failure(error): print("An error occurred: \(error)")
-//                case let .success(image): print("Successfully fetched image: \(image)")
-//                }
-//        }
-
-        // Test strategy concate
-        let test = TestConcate()
-        test.testPropertyToSignalProducer()
-
-        var a: TestSignalDeinit?
-        a = TestSignalDeinit()
-        a = nil
-
-        TestSignalAndObserver()
+        SignalProducer
+            .flatten(.concat, producers: getAvatars)
+            .on(starting: {
+                print("starting")
+            }, started: {
+                print("started")
+            }, event: { event in
+                print("event is \(event)")
+            }, completed: {
+                print("completed")
+            }, disposed: {
+                print("disposed")
+            },  value: { image in
+                print("value is \(image)")
+            })
+            .startWithResult { result in
+                switch result {
+                case let .failure(error): print("An error occurred: \(error)")
+                case let .success(image): print("Successfully fetched image: \(image)")
+                }
+        }
     }
-
-
-
 }
 
 
@@ -213,15 +220,27 @@ class TestSignalAndObserver {
 
         SignalProducer(refreshSignal)
             .on(starting: { _ in
-                isLoading.value = true
+                self.isLoading.value = true
                 print("isLoading")
+            })
+            .on(event: { event in
+                switch event {
+                case .completed:
+                    print("event completed")
+                case .value():
+                    print("event value")
+                case .failed(_):
+                    print("event error")
+                case .interrupted:
+                    print("interrupted")
+                }
+            })
+            .on(started: { _ in
+                self.isLoading.value = false
+                print("isLoading completed")
             })
             .flatMap(.latest, transform: { _ in
                 return SignalProducer(value: [Player(), Player(), Player()])
-            })
-            .on(started: { _ in
-                isLoading.value = false
-                print("isLoading completed")
             })
             .combinePrevious([])
             .startWithValues({ [weak self] (oldPlayers, newPlayers) in
@@ -244,8 +263,13 @@ class TestSignalAndObserver {
             })
 
 
-        // begin Test
+        // begin Test,
         self.refreshObserver.send(value: ())
+        self.refreshObserver.send(value: ())
+        self.refreshObserver.sendCompleted()
+
+//        self.isLoading.value = false
+//        self.isLoading.value = false
     }
 
     deinit {
@@ -259,6 +283,30 @@ class TestSignalInput {
     func test() {
         let signal = Signal<(), NoError>.pipe()
         signal.input
+    }
+}
+
+
+/// combineLatest 如果是SignalProducer，那么会立刻回调observeValues；
+/// 如果是 Signal，因为没有初始值，那么不会立刻回调，需要等待所有的signal 都产生数据才会
+class TestCombineLatest {
+    func test() {
+        let a = MutableProperty<String>("")
+        let b = MutableProperty<String>("")
+        let c = MutableProperty<String>("")
+
+        a.value = "helo"
+        b.value = "hi"
+        c.value = "e"
+
+        Signal.combineLatest(a.signal, b.signal, c.signal)
+            .observeValues { (aVal, bVal, cVal) in
+                print("combined signal = \(aVal + bVal + cVal)")
+        }
+
+        SignalProducer.combineLatest(a.producer, b.producer, c.producer).startWithValues { (aVal, bVal, cVal) in
+            print("combined producer = \(aVal + bVal + cVal)")
+        }
     }
 }
 
